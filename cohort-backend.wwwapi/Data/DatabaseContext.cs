ca133041d1602560f3350a87150a1d28cce4698e
Julia <julia.r.lindgren@gmail.com>
2024-10-17 14:10:48 +0200

    Setup db context

---
+++ cohort-backend.wwwapi/Data/DatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace cohort_backend.wwwapi.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        private string _connectionString;
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TODO: Seed data
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //public DbSet<Post> Post { get; set; }
+        //public DbSet<User> Users { get; set; }
+    }
+}
