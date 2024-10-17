384ebcb2a92118bac1a1a03efcbb56c4af595d31
John Ferdie Abueg <john.abueg@gmail.com>
2024-10-17 14:35:40 +0200

    comment model

--- cohort-backend.wwwapi/Models/Comment.cs
+++ cohort-backend.wwwapi/Models/Comment.cs
@@ -3,18 +3,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace cohort_backend.wwwapi.Models
 {
-    [Table("posts")]
-    public class Post
+    [Table("comments")]
+    public class Comment
     {
         [Key]
         [Column("id")]
         public int Id { get; set; }
 
-        [Column("title")]
-        public string Title { get; set; }
-
         [Column("content")]
         public string Content { get; set; }
+
+        [ForeignKey("posts")]
+        public int PostId { get; set; }
+        public Post Post { get; set; }
 
         [ForeignKey("users")]
         public int UserId { get; set; }
